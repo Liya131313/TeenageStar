7f89164849f3f7e72ea689b691fa2801fde21bea
Liya <liyalove1@icloud.com>
2024-06-11 09:47:10 -0500

    Initial Commit

---
+++ TeenageStar/TeenageStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeenageStarApp.swift
+//  TeenageStar
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TeenageStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
